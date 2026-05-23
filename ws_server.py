e181430642b6b15bd768cdbe9a7acf743001705a
Mike Schauber <mike.schauber96@gmail.com>
2026-05-23 22:31:08 +0200

    upload py script

---
+++ ws_server.py
@@ -0,0 +1,26 @@
+
+import asyncio
+import websockets
+import json
+
+async def handler(ws):
+    print("Client connected")
+
+    async for msg in ws:
+        try:
+            data = json.loads(msg)
+
+            throttle = data.get("throttle", 0)
+            steering = data.get("steering", 0)
+
+            print(f"Throttle: {throttle} | Steering: {steering}")
+
+        except Exception as e:
+            print("Error:", e)
+
+async def main():
+    async with websockets.serve(handler, "0.0.0.0", 8765):
+        print("WebSocket running on port 8765")
+        await asyncio.Future()
+
+asyncio.run(main())
